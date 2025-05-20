da7999148f2a166877e1b6e4aebb9bcc4fd3418b
wkdjh <wkdjh1019@gmail.com>
2025-05-20 17:16:21 +0900

    feat:로그인 회원가입 기능 추가

--- src/main/java/com/Globetrek/controller/AuthController.java
+++ src/main/java/com/Globetrek/controller/AuthController.java
@@ -1,11 +1,52 @@
 package com.Globetrek.controller;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+import com.Globetrek.dto.Request.LoginRequestDto;
+import com.Globetrek.service.AuthService;
+
+@Controller
 @RequestMapping("/auth") 
-@RequiredArgsConstructor
-public class AuthController { 
+public class AuthController {
+	
+	private final AuthService authService;
+	
+
+	@Autowired
+	public AuthController(AuthService authService) {
+		 this.authService = authService;
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	@GetMapping("/signup")
+	public String signup() {
+		return "signup";  
+	}
+
+
+	@PostMapping("/signup")
+	public String signup(LoginRequestDto loginRequestDto, Model model) {
+		try {
+			authService.registerUser(loginRequestDto);
+			return "redirect:/auth/login";
+		} catch (IllegalArgumentException e) {
+			model.addAttribute("error", e.getMessage()); // 에러 메시지 전달
+			return "signup"; // 회원가입 페이지로 다시
+		}
+	}
+	
+	@GetMapping("/test")
+	public String testPage() {
+	    return "test";  
+	}
 	
 }
