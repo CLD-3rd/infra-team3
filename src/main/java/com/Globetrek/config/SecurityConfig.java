73f4e0fde4f43e03c4c155d03e22408712990b83
deokjung <deokjung95@gmail.com>
2025-05-21 16:32:53 +0900

    mypage2

--- src/main/java/com/Globetrek/config/SecurityConfig.java
+++ src/main/java/com/Globetrek/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.Globetrek.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -22,12 +21,10 @@ public class SecurityConfig {
 	 	@Bean
 	    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 	        http
-	        	.csrf().disable()  // <-- CSRF 비활성화 (임시)
 	            .authorizeHttpRequests(authz -> authz
-	            		.requestMatchers(HttpMethod.PATCH, "/api/mypage/**").permitAll()
-	            		 .requestMatchers("/api/mypage/**").permitAll()   // 나중에 수정, 이 경로는 로그인 업이도 허용 테스트 후 비 활성화
-        				 //.anyRequest().authenticated()  // 그 외 요청은 인증 필요 테스트 후 활성화
-	            		.anyRequest().permitAll() // 모든 요청 허용 (임시) 테스트 후 활성화
+	            		 //.requestMatchers().permitAll()   // 나중에 수정, 이 경로는 로그인 업이도 허용
+        				 //.anyRequest().authenticated()  // 그 외 요청은 인증 필요 
+	            		.anyRequest().permitAll() // 모든 요청 허용 (임시)
 	                
 	            )
 	            .formLogin((form) -> form
